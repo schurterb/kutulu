6b156fae5e359251e6eac858002203fa23d599db
schurterb <schurterb@berea.edu>
2017-12-12 18:14:50 +0000

    started created arbitrage monitor

---
+++ python/kutulu/engine/arbitragemonitor.py
@@ -0,0 +1,18 @@
+"""
+@auther schurterb
+@date 17-12-12
+@description
+ The basic class for monitoring cross-exchange trends and calculating when to perform arbitrage
+"""
+
+import sys
+sys.path.append("..")
+
+import events
+
+
+
+class ArbitrageMonitor:
+    
+    def __init__(self, **kwargs):
+        pass
