06dd65e365d00f5dd33ebb390cb71d6f15c05113
schurterb <schurter.brandon@gmail.com>
2017-12-12 21:02:51 -0500

    Finished testing and debugging relative value matrix

--- python/kutulu/engine/relativevaluematrix.py
+++ python/kutulu/engine/relativevaluematrix.py
@@ -34,43 +34,47 @@ class RelativeValueMatrix:
    
     def updateRelativeValues(self, tickers):
         for ticker in tickers:
-            #Backup old data
-            old_ask = self.ask_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.base] ][ self.asset_list[ticker.quote] ]
-            old_bid = self.bid_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.base] ][ self.asset_list[ticker.quote] ]
-            old_ask_inverse = self.ask_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.quote] ][ self.asset_list[ticker.base] ]
-            old_bid_inverse = self.bid_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.quote] ][ self.asset_list[ticker.base] ]
-            try:
-                #set new data
-                self.ask_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.base] ][ self.asset_list[ticker.quote] ] = ticker.ask
-                self.bid_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.base] ][ self.asset_list[ticker.quote] ] = ticker.bid
-                self.ask_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.quote] ][ self.asset_list[ticker.base] ] = 1.0/ticker.ask
-                self.bid_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.quote] ][ self.asset_list[ticker.base] ] = 1.0/ticker.bid
-            except Exception as e:
-                #if error, restore old data
-                self.ask_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.base] ][ self.asset_list[ticker.quote] ] = old_ask
-                self.bid_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.base] ][ self.asset_list[ticker.quote] ] = old_bid
-                self.ask_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.quote] ][ self.asset_list[ticker.base] ] = old_ask_inverse
-                self.bid_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.quote] ][ self.asset_list[ticker.base] ] = old_bid_inverse
-                self.log.error("Failed to update matrix entries for exchange="+str(ticker.exchange)+" base="+str(ticker.base)+" quote="+str(ticker.quote)+".  Reason: "+str(e))
-                self.log.debug(traceback.format_exc())
+            if (ticker.ask > 0) and (ticker.bid > 0) and ticker.exchange in self.exchanges and ticker.base in self.assets and ticker.quote in self.assets:
+                #Backup old data
+                old_ask = self.ask_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.base] ][ self.asset_list[ticker.quote] ]
+                old_bid = self.bid_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.base] ][ self.asset_list[ticker.quote] ]
+                old_ask_inverse = self.ask_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.quote] ][ self.asset_list[ticker.base] ]
+                old_bid_inverse = self.bid_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.quote] ][ self.asset_list[ticker.base] ]
+                try:
+                    #set new data
+                    self.ask_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.base] ][ self.asset_list[ticker.quote] ] = ticker.ask
+                    self.bid_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.base] ][ self.asset_list[ticker.quote] ] = ticker.bid
+                    self.ask_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.quote] ][ self.asset_list[ticker.base] ] = 1.0/ticker.ask
+                    self.bid_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.quote] ][ self.asset_list[ticker.base] ] = 1.0/ticker.bid
+                except Exception as e:
+                    #if error, restore old data
+                    self.ask_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.base] ][ self.asset_list[ticker.quote] ] = old_ask
+                    self.bid_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.base] ][ self.asset_list[ticker.quote] ] = old_bid
+                    self.ask_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.quote] ][ self.asset_list[ticker.base] ] = old_ask_inverse
+                    self.bid_price_matrix[ self.exchange_list[ticker.exchange] ][ self.asset_list[ticker.quote] ][ self.asset_list[ticker.base] ] = old_bid_inverse
+                    self.log.error("Failed to update matrix entries for exchange="+str(ticker.exchange)+" base="+str(ticker.base)+" quote="+str(ticker.quote)+".  Reason: "+str(e))
+                    self.log.debug(traceback.format_exc())
     
-    def getAskPrice(self, exchange, base, quote):
+    def getAsk(self, exchange, base, quote):
         try:
             value = self.ask_price_matrix[ self.exchange_list[exchange] ][ self.asset_list[base] ][ self.asset_list[quote] ]
             if(value > 0.0):
                 return value
         except Exception as e:
-            self.log.error("Unable to retrieve an ask value for "+base+" - "+quote+" @ "+exchange+".  Reason: "+str(e))
+            self.log.error("Unable to retrieve a ask value for "+str(base)+" - "+str(quote)+" @ "+str(exchange)+".  Reason: "+str(e))
             self.log.debug(traceback.format_exc())
         return None
         
-    def getBidPrice(self, exchange, base, quote):
+    def getBid(self, exchange, base, quote):
         try:
-            value = self.bid_price_matrix[ self.exchange_list[exchange] ][ self.asset_list[base] ][ self.asset_list[quote] ]
-            if(value > 0.0):
-                return value
+            if exchange in self.exchanges and base in self.assets and quote in self.assets:
+                value = self.bid_price_matrix[ self.exchange_list[exchange] ][ self.asset_list[base] ][ self.asset_list[quote] ]
+                if(value > 0.0):
+                    return value
+            else:
+                self.log.warn(exchange+" "+base+" "+quote+" are not a valid set of options")
         except Exception as e:
-            self.log.error("Unable to retrieve a bid value for "+base+" - "+quote+" @ "+exchange+".  Reason: "+str(e))
+            self.log.error("Unable to retrieve a bid value for "+str(base)+" - "+str(quote)+" @ "+str(exchange)+".  Reason: "+str(e))
             self.log.debug(traceback.format_exc())
         return None
         
--- python/test/relativevaluematrixTest.py
+++ python/test/relativevaluematrixTest.py
@@ -9,5 +9,86 @@
 import sys
 sys.path.append("../kutulu")
 
+from events import *
+from engine.relativevaluematrix import RelativeValueMatrix
+
+exchanges = ["kraken", "gdax", "bittrex"]
+assets = ["BTC", "ETH", "LTC"]
+
+print("Initializing test")
+good_tickers = []
+for exchange in exchanges:
+    good_tickers.append(TickerData(exchange, "BTC", "ETH", 42.0, 1.0/42.0))
+    good_tickers.append(TickerData(exchange, "BTC", "LTC", 42.0, 1.0/42.0))
+    good_tickers.append(TickerData(exchange, "LTC", "ETH", 42.0, 1.0/42.0))
+    for base in assets:
+        good_tickers.append(TickerData(exchange, base, base, 1.0, 1.0))
+bad_tickers = []
+bad_tickers.append(TickerData("nasdaq", "BTC", "LTC", 42.0, 1.0/42.0))
+bad_tickers.append(TickerData(None, "LTC", "ETH", 42.0, 1.0/42.0))
+bad_tickers.append(TickerData("kraken", "COW", "ETH", 42.0, 1.0/42.0))
+bad_tickers.append(TickerData("kraken", None, "LTC", 42.0, 1.0/42.0))
+bad_tickers.append(TickerData("kraken", "BTC", "COW", 42.0, 1.0/42.0))
+bad_tickers.append(TickerData("kraken", "LTC", None, 42.0, 1.0/42.0))
+bad_tickers.append(TickerData("kraken", "BTC", "ETH", -10, 1.0/-10))
+bad_tickers.append(TickerData("kraken", "BTC", "ETH", 0.0, 0.0))
+                
+                
+print("Creating Matrix...")
+matrix = RelativeValueMatrix(exchanges, assets)
+if type(matrix) is RelativeValueMatrix:
+    print("PASS")
+else:
+    print("FAIL")
 
 
+print("Updating Matrix with good data...")
+matrix.updateRelativeValues(good_tickers)
+
+print("Checking Matrix updates...")
+check = True
+for ticker in good_tickers:
+    if((matrix.getAsk(ticker.exchange, ticker.base, ticker.quote) != ticker.ask) or
+       (matrix.getAsk(ticker.exchange, ticker.quote, ticker.base) != 1.0/ticker.ask) or
+       (matrix.getBid(ticker.exchange, ticker.base, ticker.quote) != ticker.bid) or
+       (matrix.getBid(ticker.exchange, ticker.quote, ticker.base) != 1.0/ticker.bid)):
+           print(matrix.getAsk(ticker.exchange, ticker.base, ticker.quote)," != ",ticker.ask)
+           print(matrix.getAsk(ticker.exchange, ticker.quote, ticker.base)," != ",1.0/ticker.ask)
+           print(matrix.getBid(ticker.exchange, ticker.base, ticker.quote)," != ",ticker.bid)
+           print(matrix.getBid(ticker.exchange, ticker.quote, ticker.base)," != ",1.0/ticker.bid)
+           check = False
+if check:
+    print("PASS")
+else:
+    print("FAIL")
+  
+
+print("Updating Matrix with bad data...")
+matrix.updateRelativeValues(bad_tickers)
+
+print("Checking Matrix updates...")
+check = True
+for ticker in good_tickers:
+    if((matrix.getAsk(ticker.exchange, ticker.base, ticker.quote) != ticker.ask) or
+       (matrix.getAsk(ticker.exchange, ticker.quote, ticker.base) != 1.0/ticker.ask) or
+       (matrix.getBid(ticker.exchange, ticker.base, ticker.quote) != ticker.bid) or
+       (matrix.getBid(ticker.exchange, ticker.quote, ticker.base) != 1.0/ticker.bid)):
+           print(matrix.getAsk(ticker.exchange, ticker.base, ticker.quote)," != ",ticker.ask)
+           print(matrix.getAsk(ticker.exchange, ticker.quote, ticker.base)," != ",1.0/ticker.ask)
+           print(matrix.getBid(ticker.exchange, ticker.base, ticker.quote)," != ",ticker.bid)
+           print(matrix.getBid(ticker.exchange, ticker.quote, ticker.base)," != ",1.0/ticker.bid)
+           check = False
+for ticker in bad_tickers:
+    if((matrix.getAsk(ticker.exchange, ticker.base, ticker.quote) != None) or
+       (matrix.getAsk(ticker.exchange, ticker.quote, ticker.base) != None) or
+       (matrix.getBid(ticker.exchange, ticker.base, ticker.quote) != None) or
+       (matrix.getBid(ticker.exchange, ticker.quote, ticker.base) != None)):
+           print(matrix.getAsk(ticker.exchange, ticker.base, ticker.quote)," != ",ticker.ask)
+           print(matrix.getAsk(ticker.exchange, ticker.quote, ticker.base)," != ",1.0/ticker.ask)
+           print(matrix.getBid(ticker.exchange, ticker.base, ticker.quote)," != ",ticker.bid)
+           print(matrix.getBid(ticker.exchange, ticker.quote, ticker.base)," != ",1.0/ticker.bid)
+           check = False
+if check:
+    print("PASS")
+else:
+    print("FAIL")
