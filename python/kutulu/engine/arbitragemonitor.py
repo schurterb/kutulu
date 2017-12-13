5d516f9ef211787487e5dc14fd5690cffbf5a79b
schurterb <schurterb@berea.edu>
2017-12-13 23:58:08 +0000

    Implemented main arbitrage monitor method

--- python/kutulu/engine/arbitragemonitor.py
+++ python/kutulu/engine/arbitragemonitor.py
@@ -17,15 +17,33 @@ import traceback
 
 class ArbitrageMonitor:
     
-    def __init__(self, **kwargs):
+    def __init__(self, config, **kwargs):
         self.log = logger.Logger("kutulu", "ArbitrageMonitor", "DEBUG")
+        self.network_fee = config.get("network_fees")
+        self.exchange_fee = config.get("exchange_fees")
         
-    def checkArbitrageOpportunities(self, relative_values):
+    """
+    Returns a list of arbitrage opportunities between exchanges.
+    Each list entry is formatted thus:
+        (<buy exchange> <sell exchange> <base asset> <quote asset>)
+    """
+    def checkArbitrageOpportunities(self, rvm):
+        opportunities = []
         try:
-            opportunities = []
-            
-            
+            for buy_exchange in rvm.getExchangeNames():
+                for sell_exchange in rvm.getExchangeNames():
+                    for base in rvm.getAssetNames():
+                        for quote in rvm.getAssetNames():
+                        try:
+                            buy_price = rvm.getWeightedAverage(buy_exchange, base, quote)
+                            sell_price = rvm.getWeightedAverage(sell_exchange, base, quote)
+                            if( (sell_price * self.exchange_fee[sell_exchange]) - (buy_price * self.exchange_fee[buy_exchange]) > (self.network_fee * 2) ):
+                                opportunities.append([buy_exchange, sell_exchange, base, quote])
+                        except Exception as e:
+                            self.log.error("Error while checking arbitrage between "+str(buy_exchange)+" and "+str(sell_exchange)".  Reason: "+str(e))
+                            self.log.debug(traceback.format_exc())
         except Exception as e:
             self.log.error("Failed to check for arbitrage opportunities.  Reason: "+str(e))
             self.log.debug(traceback.format_exc())
+        return opportunities
                 
