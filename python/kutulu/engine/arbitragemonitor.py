7b36639c744d5501f2e177ce95f05879d57499ef
schurterb <schurterb@berea.edu>
2017-12-20 00:26:39 +0000

    Modified ArbitrageMonitor and added test script for it

--- python/kutulu/engine/arbitragemonitor.py
+++ python/kutulu/engine/arbitragemonitor.py
@@ -34,14 +34,14 @@ class ArbitrageMonitor:
                 for sell_exchange in rvm.getExchangeNames():
                     for base in rvm.getAssetNames():
                         for quote in rvm.getAssetNames():
-                        try:
-                            buy_price = rvm.getWeightedAverage(buy_exchange, base, quote)
-                            sell_price = rvm.getWeightedAverage(sell_exchange, base, quote)
-                            if( (sell_price * self.exchange_fee[sell_exchange]) - (buy_price * self.exchange_fee[buy_exchange]) > (self.network_fee * 2) ):
-                                opportunities.append([buy_exchange, sell_exchange, base, quote])
-                        except Exception as e:
-                            self.log.error("Error while checking arbitrage between "+str(buy_exchange)+" and "+str(sell_exchange)".  Reason: "+str(e))
-                            self.log.debug(traceback.format_exc())
+                            try:
+                                buy_price = rvm.getWeightedAverage(buy_exchange, base, quote)
+                                sell_price = rvm.getWeightedAverage(sell_exchange, base, quote)
+                                if( (sell_price * self.exchange_fee[sell_exchange]) - (buy_price * self.exchange_fee[buy_exchange]) > (self.network_fee * 2) ):
+                                    opportunities.append([buy_exchange, sell_exchange, base, quote])
+                            except Exception as e:
+                                self.log.error("Error while checking arbitrage between "+str(buy_exchange)+" and "+str(sell_exchange)+".  Reason: "+str(e))
+                                self.log.debug(traceback.format_exc())
         except Exception as e:
             self.log.error("Failed to check for arbitrage opportunities.  Reason: "+str(e))
             self.log.debug(traceback.format_exc())
---
+++ python/test/arbitragemonitorTest.py
@@ -0,0 +1,54 @@
+"""
+@auther schurterb
+@date 17-12-12
+@description
+ Test script for the RelativeValueMatrix class implementation
+"""
+
+import sys
+sys.path.append("../kutulu")
+
+from events import *
+from engine.relativevaluematrix import RelativeValueMatrix
+from engine.arbitragemonitor import ArbitrageMonitor
+
+exchanges = ["kraken", "gdax", "bittrex"]
+assets = ["BTC", "ETH", "LTC"]
+
+print("Initializing test")
+good_tickers = []
+#Kraken tickers
+good_tickers.append(TickerData("kraken", "BTC", "ETH", 0.03456, 0.03445))
+good_tickers.append(TickerData("kraken", "BTC", "LTC", 0.02214, 0.02108))
+good_tickers.append(TickerData("kraken", "LTC", "ETH", 1.3895, 1.1274))
+#GDAX tickers
+good_tickers.append(TickerData("gdax", "BTC", "ETH", 0.03456, 0.03445))
+good_tickers.append(TickerData("gdax", "BTC", "LTC", 0.02214, 0.02108))
+good_tickers.append(TickerData("gdax", "LTC", "ETH", 1.3895, 1.1274))
+#Bittrex tickers
+good_tickers.append(TickerData("bittrex", "BTC", "ETH", 0.03456, 0.03445))
+good_tickers.append(TickerData("bittrex", "BTC", "LTC", 0.02214, 0.02108))
+good_tickers.append(TickerData("bittrex", "LTC", "ETH", 1.3895, 1.1274))
+#Self-exchange asset values (for initialization)
+for exchange in exchanges:
+    for base in assets:
+        good_tickers.append(TickerData(exchange, base, base, 1.0, 1.0))
+        
+rvm = RelativeValueMatrix(exchanges, assets)
+
+config = {}
+config["network_fees"] = 0.00007
+config["exchange_fees"] = 0.0026
+
+print("Creating ArbitrageMonitor")
+monitor = ArbitrageMonitor(config)
+if monitor is not None:
+    print("PASS")
+else:
+    print("FAIL")
+    
+print("Calculating Arbitrage Opportunities")
+opportunities = ArbitrageMonitor.checkArbitrageOpportunities(good_tickers)
+print(opportunities)
+
+                
